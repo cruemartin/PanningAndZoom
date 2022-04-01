bb22c7eaefe409aaeb24556b62b28fb8c609ecb0
cruemartin92 <crue.c.martin@wmich.edu>
2022-04-01 13:36:25 -0400

    Add inital classes

---
+++ src/PanningAndZoom.java
@@ -0,0 +1,8 @@
+
+public class PanningAndZoom {
+
+	public static void main(String[] args) {
+		Frame frame = new Frame();
+	}
+
+}
---
+++ src/Square.java
@@ -0,0 +1,8 @@
+import java.awt.Rectangle;
+
+public class Square extends Rectangle {
+	
+	Square(){
+		
+	}
+}
